90f0593c8c374e0a37ad05095307bcddb9d09404
Llugdar Emilio Said <93954241+EmilioSaidLlugdar@users.noreply.github.com>
2025-04-25 21:25:38 -0300

    Creacion de una Capa de Datos parte 2

---
+++ Actividades_Prácticas/BD_Python/capa_datos_persona/conexion.py
@@ -0,0 +1,48 @@
+from logger_base import log
+import psycopg2 as bd
+import sys
+
+
+class Conexion:
+    _DATABASE ='test_db'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _conexion = None
+    _cursor = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host= cls._HOST,
+                                           user= cls._USERNAME,
+                                           password = cls._PASSWORD,
+                                           port= cls._DB_PORT,
+                                           database = cls._DATABASE)
+                log.debug(f'Conexión EXITOSA: {cls._conexion}')
+                return cls._conexion #devolvemos un obj conexion que se creó correctamente
+            except Exception as e:
+                log.debug(f'Ocurrió una excepción al obtener la conexión {e}')
+                sys.exit()
+        else:
+            return cls._conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+        #se obtiene una conexion en caso que no exista
+        if cls._cursor is None:
+            try:
+                cls._cursor = cls.obtenerConexion().cursor()
+                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
+                return cls._cursor
+            except Exception as e:
+                log.error(f'Ocurrió una Excepcion al obtener el cursor: {e}')
+                sys.exit()
+        else:
+            return cls._cursor
+
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
---
+++ Actividades_Prácticas/BD_Python/capa_datos_persona/persona_dao.py
@@ -0,0 +1,45 @@
+from conexion import Conexion
+from persona import Persona
+from logger_base import log
+class PersonaDAO:
+    '''
+    DAO (Data Access Object)
+    CRUD (Create - Read - Update - Delete)
+    '''
+    _SELECCIONAR = 'SELECT * FROM persona ORDER BY id_persona'
+    _INSERTAR = 'INSERT INTO persona(nombre, apellido, email) VALUES(%s, %s, %s)'
+    _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    _ELIMINAR = 'DELETE FROM persona WHERE id_persona = %s'
+
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerCursor() as cursor:#para obtener una conexion, por eso llamamos obtenerCursor directamente
+            cursor.execute(cls._SELECCIONAR)
+            registros = cursor.fetchall() #para recuperar todos los registros
+            personas = []
+            for registro in registros:
+                persona = Persona(registro[0],registro[1],registro[2],registro[3]) #id,nombre,apellido,email
+                personas.append(persona)
+            return personas
+
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion() as conexion:
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR,valores)
+                log.debug(f'Persona insertada: {persona}')
+                return cursor.rowcount
+
+
+if __name__ == '__main__':
+    #Insertar un registro
+    persona1 = Persona(nombre='Renata', apellido='Llugdar', email='rllugdar@mail.com')
+    personas_insertadas= PersonaDAO.insertar(persona1)
+    log.debug(f'Personas Insertadas: {personas_insertadas}')
+
+
+    #seleccionar objetos
+    personas = PersonaDAO.seleccionar()
+    for persona in personas:
+        log.debug(persona)
